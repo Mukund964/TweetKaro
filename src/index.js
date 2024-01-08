896776bdf1fa24a2e8935acc32e731fcc26a4170
Mukund <0105CS201068@oriental.ac.in>
2024-01-08 20:06:04 +0530

    Tweet Repository Created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import config from './config/config.js';
 import {connect} from './config/database.js'
+import {TweetRepository} from './repository/tweetRepository.js'
 const app = express();
-
+const tweetRepo = new TweetRepository();
 const PORT = config.PORT;
 app.listen(PORT,async ()=>{
     await connect();
+    await tweetRepo.Create({
+        content : "First Tweet"
+    });
     console.log(`server is running at ${PORT}`);
 });
---
+++ src/repository/tweetRepository.js
@@ -0,0 +1,50 @@
+import tweet from '../models/tweet.js';
+
+class TweetRepository{
+    async Create(data){
+        try {
+            const response = await tweet.create(data);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async find(id){
+        try {
+            const Tweet = await tweet.findById(id);
+            return Tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getTweetWithComments(id){
+        try {
+            const Tweet = await tweet.findById(id).populate({path : 'comments'}).lean();
+            return Tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async remove(id){
+        try {
+            const res = await tweet.deleteOne(id);
+            return res;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getTweetsLimits(offset,limit){
+        try {
+            const Tweets = await tweet.find().skip(offset).limit(limit);
+            return Tweets;
+        } catch (error) {
+            console.log(error);;
+        }
+    }
+}
+
+export {TweetRepository};
