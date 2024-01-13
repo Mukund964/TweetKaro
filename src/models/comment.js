396317b4fd3f3db972e7cbaa54eabe3ff53efb76
Mukund <0105CS201068@oriental.ac.in>
2024-01-13 15:27:45 +0530

    Added Comment Model

--- src/models/comment.js
+++ src/models/comment.js
@@ -7,7 +7,21 @@ const commentSchema = new mongoose.Schema({
     },
     userId :{
         type: String
-    }
+    },
+    onModel:{
+        type : String,
+        required:true,
+        Enum : ['tweet','comment']
+    },
+    commentable:{
+        type : mongoose.Schema.Types.ObjectId,
+        require : true,
+        refPath  : "onModel"
+    },
+    comments:[{
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : 'comment'
+    }]
 },{timestamps:true});
 
 const comment = mongoose.model('Comment',commentSchema);
