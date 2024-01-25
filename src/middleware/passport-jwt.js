035ae97c9dd61def7bcf1e620760275e1793f4d0
Mukund <0105CS201068@oriental.ac.in>
2024-01-25 15:51:55 +0530

    Fixed passport middleware

--- src/middleware/passport-jwt.js
+++ src/middleware/passport-jwt.js
@@ -1,19 +1,22 @@
 import JwtStrategy from 'passport-jwt';
 import { ExtractJwt } from 'passport-jwt';
+import user from '../models/user.js';
 
-var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
+var opts = {
+    jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey : 'twitter_auth'
+}
+export const passportAuth = (passport) => {
+    passport.use(new JwtStrategy(opts, async (jwt_payload, done)=> {
+    await user.findById(jwt_payload.id, (err, User)=> {
         if (err) {
             return done(err, false);
         }
-        if (user) {
-            return done(null, user);
+        if (User) {
+            return done(null, User);
         } else {
             return done(null, false);
-            // or you could create a new account
         }
     })
 }));
+}
